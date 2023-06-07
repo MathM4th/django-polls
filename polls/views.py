2e9d9fe07b8e8a448f7b642a676b8276df6ebd33
Matheus Gomes <matheussama2019@gmail.com>
2023-06-07 09:02:30 -0300

    mudanças servidor

--- polls/views.py
+++ polls/views.py
@@ -4,5 +4,13 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Olá Mundo! Esta é a página inicial do aplicativo")
     
-def produtos(request):
-    return HttpResponse("Esta é a página dos produtos.")
+def detail(request, question_id):
+    return HttpResponse(f"Você está vendo os detalhes da questão {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Você está vendo os resultados da questão {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Você está votando na questão {question_id}")
