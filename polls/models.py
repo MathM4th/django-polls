913471f3a3becde8457e2a7cda72659247c5c80c
Matheus <rodrigues.gomes1@aluno.ifsp.edu.br>
2023-08-10 10:51:41 -0300

    criação de questions e imagens

--- polls/models.py
+++ polls/models.py
@@ -1,26 +1,63 @@
-import datetime
+#from django.db import models
 
+# # Create your models here.
+# from django.db import models
+
+# class Usuário(models.Model):
+#     nome = models.CharField(max_length=50)
+#     nickname = models.CharField(max_length=100, unique=True)
+#     avatar = models.CharField(max_length=255, null=True)
+
+#     def __str__(self):
+#         return self.nickname
+
+# class Questionário(models.Model):
+#     titulo = models.CharField(max_length=100)
+#     descricao = models.CharField(max_length=500, null=True)
+#     data_criacao = models.DateTimeField(auto_now_add=True)
+#     questionario_col = models.CharField(max_length=45, null=True)
+#     capa = models.CharField(max_length=255, null=True)
+#     usuário = models.ForeignKey(Usuário, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.titulo
+
+# class Hashtag(models.Model):
+#     tag = models.CharField(max_length=45, unique=True)
+
+#     def __str__(self):
+#         return self.tag
+
+# class Hashtag_has_questionário(models.Model):
+#     hashtag = models.ForeignKey(Hashtag, on_delete=models.CASCADE)
+#     questionário = models.ForeignKey(Questionário, on_delete=models.CASCADE)
+
+#     class Meta:
+#         unique_together = ('hashtag', 'questionário')
+
+from ast import Import
+from time import timezone
 from django.db import models
+import datetime 
 from django.utils import timezone
+
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=300)
     pub_date = models.DateTimeField("date published")
+
+    def was_published_recently(self):
+        return self.pub_date >=timezone.now() - datetime.timedelta(days=1)
+
     def __str__(self):
         return self.question_text
 
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=300)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.choice_text
-    
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
--- polls/views.py
+++ polls/views.py
@@ -1,24 +1,21 @@
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
-from django.template import loader
-
 from .models import Question
-
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
-  
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
 def detail(request, question_id):
-    return HttpResponse(f"Você está vendo os detalhes da questão {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
-    return HttpResponse(f"Você está vendo os resultados da questão {question_id}")
+    response = f'Resultados da pergunta de número {question_id}'
+    return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):
-    return HttpResponse(f"Você está votando na questão {question_id}")
+    return HttpResponse("Você está votando em uma enquete %s." % question_id)
