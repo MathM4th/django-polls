61aeca892cc001eb2313476423b8cc1b0225992b
Matheus <rodrigues.gomes1@aluno.ifsp.edu.br>
2023-08-29 11:23:20 -0300

    criação listar

--- polls/urls.py
+++ polls/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
     path("<int:question_id>/results/", views.results, name="results"),
     # ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
+
+    #class based views
+    path('listar', views.QuestionListView.as_view(), name="question-list"),
+    path('cadastrar', views.QuestionCreateView.as_view(), name="question-create")
 ]
