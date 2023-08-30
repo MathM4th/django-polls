7822fb106722f5674c8de337f80f38202fb8bcfe
Matheus <rodrigues.gomes1@aluno.ifsp.edu.br>
2023-08-30 09:28:06 -0300

    detalhes

--- polls/urls.py
+++ polls/urls.py
@@ -4,15 +4,16 @@ from . import views
 
 urlpatterns = [
     # ex: /polls/
-    path("", views.index, name="index"),
+    #path("", views.index, name="index"),
     # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
+    #path("<int:question_id>/", views.detail, name="detail"),
     # ex: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
+    #path("<int:question_id>/results/", views.results, name="results"),
     # ex: /polls/5/vote/
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+    #path("<int:question_id>/vote/", views.vote, name="vote"),
 
     #class based views
     path('listar', views.QuestionListView.as_view(), name="question-list"),
-    path('cadastrar', views.QuestionCreateView.as_view(), name="question-create")
+    path('cadastrar', views.QuestionCreateView.as_view(), name="question-create"),
+    path('<int:pk>', views.QuestionDetailView.as_view(), name="question-detail")
 ]
