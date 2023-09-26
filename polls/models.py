82844b2dce2875f17a67dd47116f613b4ad15fd4
Matheus <rodrigues.gomes1@aluno.ifsp.edu.br>
2023-09-26 11:27:31 -0300

    login e signup

--- polls/models.py
+++ polls/models.py
@@ -61,3 +61,12 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+class Usuario(models.Model):
+    data_nascimento = models.DateField("Data de Nascimento")
+    cpf = models.CharField("CPF", unique=True, max_length=11)
+    endereco = models.CharField("Endereço Completo", max_length=255)
+
+class Doacao(models.Model):
+    valor = models.DecimalField("Valor da doação", max_digits=10, decimal_places=2)
+    confirmacao = models.BooleanField("Pagamento confirmado?", default=False)
