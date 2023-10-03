f8a141316a8b15020d5d63ad59cd06d9685bd4b6
Matheus Rodrigues <rodrigues.gomes1@aluno.ifsp.edu.br>
2023-10-03 11:26:11 -0300

    registrar, acessar e sair

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,4 +8,9 @@ class AccountSignupForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', )
+        fields = ('username', 'email', 'data_nascimento', 'cpf', 'password', )
+        widgets = {
+            'data_nascimento':forms.widgets.DateInput(
+                attrs={'type':'date','required':'required'}
+            )
+        }
