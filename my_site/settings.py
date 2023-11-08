328aa5d7a4a835512a19ace1d09c2ad95123f39a
Matheus <rodrigues.gomes1@aluno.ifsp.edu.br>
2023-11-07 23:39:47 -0300

    Registro de usuários com novos campos e metade do upload de arquivos - foto de perfil

--- my_site/settings.py
+++ my_site/settings.py
@@ -131,3 +131,6 @@ LOGIN_REDIRECT_URL = '/enquetes/listar'
 LOGOUT_REDIRECT_URL = '/enquetes/listar'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'files/')
--- my_site/urls.py
+++ my_site/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("enquetes/", include("polls.urls")),
     path("admin/", admin.site.urls),
     path('', include('accounts.urls')), # rotas personalizadas como accounts/signup
     path('accounts/', include('django.contrib.auth.urls')), # rotas padrão fornecida pelo Django
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
